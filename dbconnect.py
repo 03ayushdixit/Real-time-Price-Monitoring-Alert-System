84bd43d2e91a20e3c62c5e19360e37aa21397e2a
unknown <ayushlaptop03@gmail.com>
2026-04-24 12:57:14 +0530

    successfully uploaded

---
+++ dbconnect.py
@@ -0,0 +1,13 @@
+import mysql
+import mysql.connector as sql
+
+db=sql.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    use_pure=True
+)
+cr= db.cursor()
+cr.execute("show databases")
+for i in cr:
+    print(i)
--- interface.py
+++ interface.py
@@ -1,3 +1,31 @@
 import streamlit as st
+from PIL import Image 
+
+
+img = Image.open("logo.jpg")
+
+st.image(img,width=600)
 
 st.title("real time price analysis App")
+
+st.header("Daily Price Tracking")
+st.header("Historical Storage")
+st.header("Price Comparison")
+st.header("Alert System for price changes")
+st.header("Automated pipeline")
+
+## markdown use for header size increasse decrease(no of # inc so size decrease vice-versa)
+st.markdown("### Database Connective")  
+
+st.success("Proceed Successfully")
+st.info("Find the formal information")
+st.warning("you are going to high risk scenerio")
+st.error("Account has been locked for 24 hours")
+
+
+img = Image.open("logo1.jpg")
+
+st.image(img,width=600)
+
+
+
